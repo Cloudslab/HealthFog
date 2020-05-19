c454497d39fb86f71bbdd370a7d778ca35f43daf
shreshthtuli <shreshthtuli@gmail.com>
2020-05-20 00:53:46 +0530

    Bug fix

--- upload.php
+++ upload.php
@@ -5,6 +5,8 @@
 
 <?php
    $data = $_GET['data'];
-   if (!(file_put_contents('data.csv',$data) === FALSE)) echo "File xfer completed."; // file could be empty, though
+   if (!(file_put_contents('HeartModel/data.csv',$data) === FALSE)){
+   	echo "File xfer completed."; // file could be empty, though
+   }
    else echo "File xfer failed.";
 ?>
